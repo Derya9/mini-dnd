c4bf367f8ed959a8a2501b6cba43856ce8e43d51
Guillermo Herrero (EL) <guillermo.herrero@codecool.com>
2023-04-05 16:15:40 +0200

    Start the new project

---
+++ backend/src/main/java/me/derya/minidnd/MinidndApplication.java
@@ -0,0 +1,13 @@
+package me.derya.minidnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinidndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinidndApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/me/derya/minidnd/MinidndApplicationTests.java
@@ -0,0 +1,13 @@
+package me.derya.minidnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinidndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
